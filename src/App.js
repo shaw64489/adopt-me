c1ad8f150e1dff11757ea19f9d2a406f3d427a14
Chris Shaw <shaw64489@yahoo.com>
2020-10-12 15:26:16 -0400

    pet api - search params, dropsdown, pet components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel',
-    }),
-    React.createElement(Pet, {
-      name: 'Ralph',
-      animal: 'Cat',
-      breed: 'Ugly',
-    }),
-  ]);
+  return (
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
+  );
 };
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
